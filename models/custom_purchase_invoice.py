0a002374b8a36b833adf5bb56b7b8e5a37f7db70
tatendatembojnr-code <tatendatembojnr-code@users.noreply.github.com>
2026-08-08 09:52:45 +0200

    fix: change uom_po_id to uom_id on product.product fallback

--- models/custom_purchase_invoice.py
+++ models/custom_purchase_invoice.py
@@ -75,7 +75,7 @@ class CustomPurchaseInvoice(models.Model):
                         'product_id': line.product_id.id,
                         'name': line.name or line.product_id.name,
                         'quantity': line.product_qty,
-                        'product_uom_id': line.product_uom_id.id if line.product_uom_id else line.product_id.uom_po_id.id,
+                        'product_uom_id': line.product_uom_id.id if line.product_uom_id else line.product_id.uom_id.id,
                         'price_unit': line.price_unit,
                         'tax_ids': [(6, 0, line.taxes_id.ids)] if line.taxes_id else False,
                     }))
--- models/custom_supplier_quote.py
+++ models/custom_supplier_quote.py
@@ -68,7 +68,7 @@ class CustomSupplierQuote(models.Model):
                         'product_id': line.product_id.id,
                         'name': line.name or line.product_id.name,
                         'product_qty': line.product_qty,
-                        'product_uom_id': line.product_uom_id.id if line.product_uom_id else line.product_id.uom_po_id.id,
+                        'product_uom_id': line.product_uom_id.id if line.product_uom_id else line.product_id.uom_id.id,
                         'price_unit': line.price_unit,
                         'tax_ids': [(6, 0, line.taxes_id.ids)] if line.taxes_id else False,
                     }))
@@ -83,7 +83,7 @@ class CustomSupplierQuote(models.Model):
                         'product_id': line.product_id.id,
                         'name': line.name or line.product_id.name,
                         'product_qty': line.product_qty,
-                        'product_uom_id': line.product_uom_id.id if line.product_uom_id else line.product_id.uom_po_id.id,
+                        'product_uom_id': line.product_uom_id.id if line.product_uom_id else line.product_id.uom_id.id,
                         'price_unit': line.price_unit,
                         'tax_ids': [(6, 0, line.taxes_id.ids)] if line.taxes_id else False,
                     }))
